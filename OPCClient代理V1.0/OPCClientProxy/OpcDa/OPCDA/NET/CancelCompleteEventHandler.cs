cebadc4e81b78f15e2732de844cf489fb6b0c90f
james <blursj@gmail.com>
2016-08-12 13:58:06 +0800

    close project

--- OPCClient代理V1.0/OPCClientProxy/OpcDa/OPCDA/NET/CancelCompleteEventHandler.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OPCDA.NET
-{
-    using System;
-    using System.Runtime.CompilerServices;
-
-    public delegate void CancelCompleteEventHandler(object sender, CancelCompleteEventArgs e);
-}
-
